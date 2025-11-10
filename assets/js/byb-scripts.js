563cccef9fb79d1405f6827d33c2e743eb1d5e90
Devwytlabs <dev@wytlabs.com>
2025-11-10 17:40:44 +0530

    Fix: ui fix

--- assets/js/byb-scripts.js
+++ assets/js/byb-scripts.js
@@ -126,9 +126,6 @@ document.addEventListener('DOMContentLoaded', () => {
         async loadProducts() {
 
             const category = document.querySelector('input[name="byb-category-filter"]:checked')?.value || '';
-
-            console.log(category);
-
             const sort = document.querySelector('input[name="byb-sort"]:checked')?.value || '';
             const search = document.getElementById('byb-search')?.value || '';
 
